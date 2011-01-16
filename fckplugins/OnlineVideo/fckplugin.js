dd4e8be8149d957e63f5ce32a094a65836b5c804
Michael James <mike@mjjames.co.uk>
2011-01-16 19:04:16 +0000

    new SimpleImage plugin , makes inserting images, linking them etc easier bug fix to the onlinevideo plugin, removed the languages stuff and made it explicit, context menu is correctly bound to images that are flash not normal images

--- fckplugins/OnlineVideo/fckplugin.js
+++ fckplugins/OnlineVideo/fckplugin.js
@@ -6,10 +6,23 @@
 */
 
 // Register the related commands.
-FCKCommands.RegisterCommand('OnlineVideo', new FCKDialogCommand(FCKLang['DlgOnlineVideoTitle'], FCKLang['DlgOnlineVideoTitle'], FCKConfig.PluginsPath + 'OnlineVideo/OnlineVideo.html', 450, 350));
+FCKCommands.RegisterCommand('OnlineVideo', new FCKDialogCommand("Online Video", "Online Video", FCKConfig.PluginsPath + 'OnlineVideo/OnlineVideo.html', 450, 350));
 
 // Create the "OnlineVideo" toolbar button.
-var oFindItem		= new FCKToolbarButton( 'OnlineVideo', FCKLang['OnlineVideoTip'] ) ;
+var oFindItem		= new FCKToolbarButton( 'OnlineVideo', "Online Video" ) ;
 oFindItem.IconPath	= FCKConfig.PluginsPath + 'OnlineVideo/OnlineVideo.gif' ;
 
 FCKToolbarItems.RegisterItem( 'OnlineVideo', oFindItem ) ;			// 'OnlineVideo' is the name used in the Toolbar config.
+FCK.ContextMenu.RegisterListener({
+    AddItems: function (menu, tag, tagName) {
+        var oFakeImage = FCK.Selection.GetSelectedElement();
+        // under what circumstances do we display this option
+        if (tagName == 'IMG' && oFakeImage.getAttribute('_fckflash')) {
+            // when the option is displayed, show a separator  the command
+            menu.AddSeparator();
+            // the command needs the registered command name, the title for the context menu, and the icon path
+            menu.AddItem('OnlineVideo', "Online Video Properties", oFindItem.IconPath);
+        }
+    } 
+}
+);
---
+++ fckplugins/SimpleImage/fckplugin.js
@@ -0,0 +1,29 @@
+﻿/*
+*Image Uploader with MJJames Simple Interface
+@author mjjames mike@mjjames.co.uk
+*/
+
+
+// Register the related commands.
+FCKCommands.RegisterCommand('SimpleImage', new FCKDialogCommand(FCKLang['DlgImgTitle'], FCKLang['DlgImgTitle'], FCKConfig.PluginsPath + 'SimpleImage/SimpleImage.html', 340, 400));
+
+// Create the "Watermark" toolbar button.
+var oSimpleImageItem = new FCKToolbarButton('SimpleImage', FCKLang['DlgImgTitle']);
+oSimpleImageItem.IconPath = FCKConfig.PluginsPath + 'SimpleImage/SimpleImage.gif';
+
+FCKToolbarItems.RegisterItem('SimpleImage', oSimpleImageItem);	
+
+FCK.ContextMenu.RegisterListener( {
+        AddItems : function( menu, tag, tagName )
+        {
+            // under what circumstances do we display this option
+            var oFakeImage = FCK.Selection.GetSelectedElement();
+            if (tagName == 'IMG' && !oFakeImage.getAttribute('_fckflash')) 
+            {
+                // when the option is displayed, show a separator  the command
+                menu.AddSeparator() ;
+                // the command needs the registered command name, the title for the context menu, and the icon path
+                menu.AddItem('SimpleImage', "Image Properties", oSimpleImageItem.IconPath);
+            }
+        }}
+);

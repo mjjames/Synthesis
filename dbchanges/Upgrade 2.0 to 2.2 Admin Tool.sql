4375c94ee1674a029d4e1c1ac21d3ca249991791
Michael James <michael.james@mksoftwaresolutions.co.uk>
2014-01-01 13:08:45 +0000

    articles, offers, news now have page title and meta description

---
+++ dbchanges/Upgrade 2.0 to 2.2 Admin Tool.sql
@@ -0,0 +1,11 @@
+alter table [dbo].[offers]
+add pagetitle nvarchar(max) null,
+metadescription nvarchar(max) null
+
+alter table [dbo].[projects]
+add pagetitle nvarchar(max) null,
+metadescription nvarchar(max) null
+
+alter table [dbo].[articles]
+add pagetitle nvarchar(max) null,
+metadescription nvarchar(max) null

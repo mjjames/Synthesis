72ebb3c568fd28d872dde4d97b13806590f21252
Michael James <michael.james@mksoftwaresolutions.co.uk>
2014-04-16 19:12:45 -0100

    dbchanges were in the wrong file

--- dbchanges/Upgrade 2.2 to 2.3 Admin Tool.sql
+++ dbchanges/Upgrade 2.2 to 2.3 Admin Tool.sql
@@ -8,35 +8,3 @@ alter column thumbnailimage nvarchar(255)
 alter table [dbo].[articles]
 alter column thumbnailimage nvarchar(255)
 
-alter table [dbo].[sites]
-add password nvarchar(255) null
-
-insert into [dbo].[lookup]
-([lookup_id], [title], [type], [value], [active])
-values
-('sitelookup','Site Lookup','lookup_type','sitelookup',1)
-
-insert into [dbo].[lookup]
-([lookup_id], [title], [type], [value], [active])
-values
-('pagelookup','Page Lookup','lookup_type','pagelookup',1)
-
-insert into [dbo].[lookup]
-([lookup_id], [title], [type], [value], [active])
-values
-('projectlookup','Project Lookup','lookup_type','projectlookup',1)
-
-insert into [dbo].[lookup]
-([lookup_id], [title], [type], [value], [active])
-values
-('articlelookup','Article Lookup','lookup_type','articlelookup',1)
-
-insert into [dbo].[lookup]
-([lookup_id], [title], [type], [value], [active])
-values
-('medialookup','Media Lookup','lookup_type','medialookup',1)
-
-insert into [dbo].[lookup]
-([lookup_id], [title], [type], [value], [active])
-values
-('marketingsitelookup','Marketing Site Lookup','lookup_type','marketingsitelookup',1)

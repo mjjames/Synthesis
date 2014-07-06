2effd3c2ac5a60064076ca4c4e47fafab84b1c36
Michael James <michael.james@mksoftwaresolutions.co.uk>
2014-07-06 16:11:07 -0100

    make key values case insensitivie

--- Repositories/KeyValueRepository.cs
+++ Repositories/KeyValueRepository.cs
@@ -16,9 +16,9 @@ namespace mjjames.AdminSystem.Repositories
 		public string GetKeyValue(string lookupID, int linkKey, string linktypeID)
 		{
 			var data = (from kv in _admin.keyvalues
-			            where kv.lookup.lookup_id == lookupID
+			            where kv.lookup.lookup_id.ToLower() == lookupID.ToLower()
 			                  && kv.link_fkey == linkKey
-			                  && kv.lookup1.lookup_id == linktypeID
+			                  && kv.lookup1.lookup_id.ToLower() == linktypeID.ToLower()
 			            select kv).FirstOrDefault();
 			return data != null ? data.value : String.Empty;
 		}
@@ -29,9 +29,9 @@ namespace mjjames.AdminSystem.Repositories
 			//we actually stash them away in a dictionary,the key made up from the values we pass in as it's easier to match them up
 			var updateData = (from kv in _admin.keyvalues
 			                 where data.Select(k => k.LinkKey).Contains(kv.link_fkey) 
-							 	&& data.Select(k => k.LinkTypeID).Contains(kv.lookup1.lookup_id)
-			                       && data.Select(k => k.LookupID).Contains(kv.lookup.lookup_id)
-			                 select kv).ToDictionary(kv => String.Format("{0}-{1}-{2}", kv.link_fkey, kv.lookup.lookup_id, kv.lookup1.lookup_id),
+							 	&& data.Select(k => k.LinkTypeID.ToLower()).Contains(kv.lookup1.lookup_id.ToLower())
+			                       && data.Select(k => k.LookupID.ToLower()).Contains(kv.lookup.lookup_id.ToLower())
+			                 select kv).ToDictionary(kv => String.Format("{0}-{1}-{2}", kv.link_fkey, kv.lookup.lookup_id.ToLower(), kv.lookup1.lookup_id.ToLower()),
 												 kv => kv);
 			
 			var dataToAdd = new List<KeyValueData>();
@@ -39,7 +39,7 @@ namespace mjjames.AdminSystem.Repositories
 			//then update the values and write them away
 			foreach (var keyvalue in data)
 			{
-				var key = String.Format("{0}-{1}-{2}", keyvalue.LinkKey, keyvalue.LookupID, keyvalue.LinkTypeID);
+				var key = String.Format("{0}-{1}-{2}", keyvalue.LinkKey, keyvalue.LookupID.ToLower(), keyvalue.LinkTypeID.ToLower());
 				//if we dont contain the data it must be a new item so save it
 				if(!updateData.ContainsKey(key))
 				{
@@ -62,14 +62,14 @@ namespace mjjames.AdminSystem.Repositories
 			//we have to lookup the keys of the lookup id's, however rather than look this up for each of them we should look all of them up
 			//stash them in a lookup and then use them upon insert
 
-			var lookupIDs = data.Select(k => k.LinkTypeID).Union(data.Select(k => k.LookupID)).ToList();
+			var lookupIDs = data.Select(k => k.LinkTypeID.ToLower()).Union(data.Select(k => k.LookupID.ToLower())).ToList();
 			var lookupDict = (from l in _admin.lookups
-			                 where lookupIDs.Contains(l.lookup_id)
+			                 where lookupIDs.Contains(l.lookup_id.ToLower())
 			                 select new
 			                        	{
 			                        		l.lookup_id,
 			                        		l.lookup_key
-			                        	}).ToDictionary(k => k.lookup_id, k => k.lookup_key);
+			                        	}).ToDictionary(k => k.lookup_id.ToLower(), k => k.lookup_key);
 
 			//now build new keyvalue objects to actually insert into the db 
 
@@ -77,8 +77,8 @@ namespace mjjames.AdminSystem.Repositories
 			                                    	{
 			                                    		link_fkey = k.LinkKey,
 			                                    		value = k.Value,
-			                                    		key_lookup = lookupDict[k.LinkTypeID],
-			                                    		link_lookup = lookupDict[k.LookupID]
+			                                    		key_lookup = lookupDict[k.LinkTypeID.ToLower()],
+			                                    		link_lookup = lookupDict[k.LookupID.ToLower()]
 			                                    	});
 
 			_admin.keyvalues.InsertAllOnSubmit(dataToInsert); 
